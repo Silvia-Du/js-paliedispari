f613ee64b46091f624e144fd7f6dc368f90e57f7
Silvia-Du <silviadu@hotmail.it>
2022-04-20 17:09:57 +0200

    fine provvisoria Parole palindrome

--- Palindroma/script.js
+++ Palindroma/script.js
@@ -6,18 +6,15 @@ Bonus: L’inserimento avviene tramite un campo input
 
 
 let checkWord = false;
-let checkPali;
+
 let message;
 let userWord;
-let letter;
 let halfPastLetter;
 
 //verifico che che sia una sola parola.
 while(!checkWord){
 
-  //userWord = prompt('inserisci una parola per vedere se è palindroma').trim();
-  //userWord = 'scorrere';
-  userWord = 'onorarono';
+  userWord = 'onorarono';// prompt
   const wordCounter = userWord.split(' ');
 
   if(wordCounter.length != 1) {
@@ -27,46 +24,35 @@ while(!checkWord){
   }
 }
 
-
-
-for(let i = 0; i< userWord.length / 2; i++){
-  letter = userWord.charAt(i);
-    // console.log(letter, i);
-}
-  
-for(let i = userWord.length -1; i> (userWord.length / 2) -1; i--){
-  halfPastLetter = userWord.charAt(i);
-    // console.log(halfPastLetter, i);
-}
-
-if(letter === halfPastLetter){
-  console.log('lettera uguale',letter,'-', halfPastLetter);
+//assegno una let alla function;
+if( isPalindroma( userWord )){
+  message = `"${userWord}" è palindroma`;
 }else{
-  console.log('lettera diversa');
+  message = 'non è mica palindroma';
 }
 
+//function
 
+function isPalindroma( userWord ){
 
+  let checkPali = true;
 
+  for( let i = 0; i < userWord.length / 2; i++ ){
 
+    let letter = userWord.charAt( i );
+    let fine = userWord.length - 1;
+    console.log(fine, 'fine');
 
- 
+    let letter2 = userWord.charAt( fine - i );
+    console.log(letter, '--', letter2);
 
+    if( letter != letter2 ){
+     checkPali = false;
+    }
+  }
 
-
-
+  return checkPali;
+}
 
 
 document.getElementById('output').innerHTML = message;
-
-// fino a metà passo le lettere come in un Array, 
-// l'altra metà la faccio scorrere al contrario e la salvo in un altro array
-// guardo se gli elementi degli array sono uguali.
-
-
-
-/*
-function isPalindroma(parola){
-
-  return message
-}*/
