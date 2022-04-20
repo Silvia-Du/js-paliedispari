d85707c7fdeae40b61fa8b80e7999242a8e828a5
Silvia-Du <silviadu@hotmail.it>
2022-04-20 15:14:51 +0200

    Prole palindrome: inizio verifica della parola

--- Palindroma/script.js
+++ Palindroma/script.js
@@ -4,3 +4,47 @@ Creare una funzione per capire se la parola inserita è palindroma
 Bonus: L’inserimento avviene tramite un campo input
 */
 
+
+let checkWord = false;
+let message;
+let userWord;
+
+//verifico che che sia una sola parola.
+while(!checkWord){
+
+  //userWord = prompt('inserisci una parola per vedere se è palindroma').trim();
+  userWord = 'scorrere';
+  const wordCounter = userWord.split(' ');
+
+  if(wordCounter.length != 1) {
+    alert('inserisci solo una parola!')
+  }else{
+    checkWord = true;
+  }
+}
+
+//se non è divisibile per 2 non può essere palindroma
+if(userWord.length % 2){
+  message = 'la parola non è palindroma';
+}
+
+for(let i = 0; i< userWord.length / 2; i++){
+  let letter = userWord.charAt(i);
+  console.log(letter);
+}
+
+
+
+document.getElementById('output').innerHTML = message;
+
+// fino a metà passo le lettere come in un Array, 
+// l'altra metà la faccio scorrere al contrario e la salvo in un altro array
+// guardo se gli elementi degli array sono uguali.
+
+
+
+/*
+function isPalindroma(parola){
+
+  return message
+}*/
