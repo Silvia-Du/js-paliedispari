6d1d700960d18fad1b463dc58866501d8dfb37b0
Silvia-Du <silviadu@hotmail.it>
2022-04-20 17:30:38 +0200

    Inizio Pari Dispari function

--- Pari e Dispari/script.js
+++ Pari e Dispari/script.js
@@ -7,3 +7,16 @@ Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 Dichiariamo chi ha vinto.
 Bonus: L’inserimento avviene tramite un campo input
 */
+let userPariDispari = 'pari'; //prompt
+let pcNumber;
+let min = 1;
+let max = 5;
+
+let userNumber = 5; //prompt
+//verificherò che sia tra 1 e 5 e che sia un numero 
+
+function pcRandomNumber(min, max){
+
+  return  Math.floor(Math.random() * (max - min +1) + min );
+
+}
